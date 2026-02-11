989f9ead8f18aaa4982fb8282fdf87d23f385878
Prabha Adhitya <prabhaadhitya01@gmail.com>
2026-02-11 14:14:51 +0530

    Changed RateLimit Middleware

--- src/middlewares/rateLimitMiddleware.js
+++ src/middlewares/rateLimitMiddleware.js
@@ -1,26 +1,3 @@
-// const { incr } = require('../utils/cache');
-
-// const rateLimit = (prefix, limit = 5, windowSeconds = 600) => {
-//     return async (req, res, next) => {
-//         try {
-//             const clientIp = req.ip || "unknown";
-//             const key = `ratelimit:${prefix}:${clientIp}`;
-//             const count = await incr(key, windowSeconds);
-//             if (count === null) {
-//                 return next();
-//             }
-//             if (count > limit) {
-//                 return res.status(429).json({ message: 'Too many requests. Please try again later.' });
-//             }
-//             res.locals.rateLimitCount = { count, limit, windowSeconds };
-//             next();
-//         } catch (error) {
-//             console.log('Rate Limiter Middleware Error:', error);
-//             next();
-//         }
-//     }
-// };
-
 const { client } = require('../config/redis');
 
 const rateLimit = async (req, res, next) => {
